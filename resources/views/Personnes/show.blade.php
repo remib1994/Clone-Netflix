01be6efd26c0c7a674869d040763eac3286cfa30
Remi Berneche <remib1994@gmail.com>
2023-11-02 15:48:06 -0400

    modifs

--- resources/views/Personnes/show.blade.php
+++ resources/views/Personnes/show.blade.php
@@ -16,10 +16,14 @@
                             <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="{{$personne->urlphoto}}" alt="">
                             <div class="flex flex-col justify-between p-4 leading-normal">
                                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$personne->prenom}} {{$personne->nom}}</h5>
-                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">date de naissance : {{$personne->date_naissance}}<br>
-                                    nationalite : {{$personne->nationalite}}<br>
-                                    sexe : {{$personne->sexe}}<br>
-                                    rôle : {{$personne->role}}<br></p>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                date de naissance : {{$personne->date_naissance}}<br>
+                                nationalite : {{$personne->nationalite}}<br>
+                                sexe : {{$personne->sexe}}<br>
+                                rôle : {{$personne->role}}<br>
+                                Acteur : {{$personne->filmsA->count()}} films <br>
+                                Producteur : {{$personne->filmsP->count()}} films <br>
+                                Réalisateur : {{$personne->filmsR->count()}} films <br>
                             </div>
                         </a>
 
