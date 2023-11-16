4a53a96acaa89a6a81aac7d4c5026c5a1e4a3e6c
tritri <64111002+trimikix20@users.noreply.github.com>
2023-11-16 20:43:01 +0000

    login page

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -6,8 +6,22 @@
     <section class="main-container " >
         <div class="text-center">
             <h1 class="text-white">Se connecter</h1>
-
         </div>
+
+        <form method="post" action="{{route('login')}}">
+            @csrf
+            <div class="form-group">
+                <label for="email" class="text-white">Email</label>
+                <input type="email" name="email" id="email" class="form-control red" placeholder="email" >
+            </div>
+            <div class="form-group">
+                <label for="password" class="text-white">Mot de passe</label>
+                <input type="password" name="password" id="password" class="form-control red" placeholder="Mot de passe" >
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Se connecter</button>
+            </div>
+
 
 
 
